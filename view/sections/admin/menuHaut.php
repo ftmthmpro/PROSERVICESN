b82843297a25b2b6b5df34af6fa67332fac8a137
ftmthmpro <ftmthmpro@gmail.com>
2026-06-19 17:39:13 +0300

    second commit

--- view/sections/admin/menuHaut.php
+++ view/sections/admin/menuHaut.php
@@ -1,7 +1,7 @@
 <div id="header" class="header navbar-default">
 	<!-- begin navbar-header -->
 	<div class="navbar-header">
-		<a href="index.html" class="navbar-brand"><span class="navbar-logo"></span> <b>PROSERVICE</b>SN</a>
+		<a href="admin.php" class="navbar-brand"><span class="navbar-logo"></span> <b>PROSERVICE</b>SN</a>
 		<button type="button" class="navbar-toggle" data-click="sidebar-toggled">
 			<span class="icon-bar"></span>
 			<span class="icon-bar"></span>
